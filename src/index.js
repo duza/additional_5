32f5a51ad536ff02047a87244994de1121efd276
duza <duza@tut.by>
2018-09-30 02:35:14 +0300

    Add first incorrect implementation of algorithm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const bracket = str[i];
+    const isOpening = bracketsConfig.map(subArray => subArray[0]).includes(bracket);
+    if (isOpening) {
+      stack.push(bracket);
+    } else {
+      const closingConfigBrackets = bracketsConfig.map(subArray => subArray[1]);
+      const isClosing = closingConfigBrackets.includes(bracket);
+      if (!stack.length && isClosing) {
+        return false;
+      } else if (isClosing) {
+        const top = stack[stack.length = 1];
+        const indexOfSubArrClosing = closingConfigBrackets.indexOf(bracket);
+        if (top === bracketsConfig[indexOfSubArrClosing][0]) {
+          stack.pop();
+        }
+      }
+    }
+  };
+  return stack.length === 0;
 }
