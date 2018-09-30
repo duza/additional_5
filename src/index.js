cba46bf1e9242a3003a88eb0723b46c6860cee9d
duza <duza@tut.by>
2018-09-30 12:54:01 +0300

    Fix solution, add handling exceptional cases

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ module.exports = function check(str, bracketsConfig) {
   const stack = [];
   for (let i = 0; i < str.length; i++) {
     const bracket = str[i];
+    const closingConfigBrackets = bracketsConfig.map(subArray => subArray[1]);
     const isOpening = bracketsConfig.map(subArray => subArray[0]).includes(bracket);
-    if (isOpening) {
-      stack.push(bracket);
-    } else {
-      const closingConfigBrackets = bracketsConfig.map(subArray => subArray[1]);
-      const isClosing = closingConfigBrackets.includes(bracket);
-      if (!stack.length && isClosing) {
-        return false;
-      } else if (isClosing) {
-        const top = stack[stack.length = 1];
+    const isClosing = closingConfigBrackets.includes(bracket);
+    if (stack.length) {
+      if (isClosing) {
+        const top = stack[stack.length - 1];
         const indexOfSubArrClosing = closingConfigBrackets.indexOf(bracket);
         if (top === bracketsConfig[indexOfSubArrClosing][0]) {
           stack.pop();
+        } else if (isOpening) {
+          stack.push(bracket);
         }
+      } else if (isOpening) {
+        stack.push(bracket);
+      }
+    } else {
+      if (isOpening) {
+        stack.push(bracket);
+      } else if (isClosing) {
+        return false;
       }
     }
   };
-  return stack.length === 0;
+  return !stack.length;
 }
